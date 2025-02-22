3e79c3713ad561d76963fff3a3d28ced9b536f8d
dajerm <daj@arkitech.no>
2025-02-22 13:38:15 +0100

    fix

--- quasar.config.ts
+++ quasar.config.ts
@@ -51,6 +51,7 @@ export default defineConfig((/* ctx */) => {
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
       // publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' ? '/PocketGames/' : '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
